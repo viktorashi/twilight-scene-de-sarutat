ef8f7aa1c1f24a116eb579a2f30a0210303fcfef
viktorashi <ioanvictorstan@gmail.com>
2025-01-08 01:58:01 +0200

    ar trebui sa mearga ig

--- adicapebuine.py
+++ adicapebuine.py
@@ -1,7 +1,22 @@
-from playsound import playsound
-import sys
 from gtts import gTTS
+import pygame
+from io import BytesIO
+from playsound import playsound
+from sys import argv
+
+
+def say(text):
+    tts = gTTS(text=text, lang='ro')
+    fp = BytesIO()
+    tts.write_to_fp(fp)
+    fp.seek(0)
+    pygame.mixer.init()
+    pygame.mixer.music.load(fp)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
 
 playsound('part1.mp3')
-playsound(str(argv[1]))
+say(argv[1])
 playsound('part2.mp3')
